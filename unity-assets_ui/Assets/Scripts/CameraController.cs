451d4a6c9afb1924ac431c8b29690fc53695e3d3
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-06-06 04:13:09 +0200

    Task 0

---
+++ unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+public float sensitivity = 5;
+public Transform player;
+private Vector3 offset;
+
+    // Update is called once per frame
+void Update ()
+	{
+        transform.position = player.transform.position + new Vector3(0, 1, -10);
+    }
+
+void FixedUpdate ()
+    {
+		//with no right clic pressed
+		offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * sensitivity, Vector3.up) * offset;
+		Vector3 newPosition = player.position + offset;
+		transform.position = newPosition;
+        transform.LookAt(player.position);
+		player.Rotate(Input.GetAxis("Mouse X") * sensitivity * Vector3.up);
+		//if (Input.GetMouseButton(0)) use when the right clic is pressed
+		//{
+		//	float rotateHorizontal = Input.GetAxis ("Mouse X");
+        //	transform.Rotate(player.transform.position * rotateHorizontal * sensitivity);
+
+		//}
+
+    }
+}
---
+++ unity-assets_ui/Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+	public Text TimerText;
+    private float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+        TimerText.text = string.Format("{0:0}:{1:00}.{2:00}", timer / 60, timer % 60, timer * 100 % 100);
+	}
+}
