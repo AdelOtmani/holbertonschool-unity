fd6bb8607afe37d06ee2837af04a9924327a3186
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-07-10 02:04:12 +0200

    test

---
+++ unity-animation/Assets/Scripts/Timer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Diagnostics;
+using System;
+
+public class Timer : MonoBehaviour {
+
+	public Text TimerText;
+    public Text FinalTime;
+    public static Stopwatch timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = new Stopwatch();
+        timer.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeSpan ts = timer.Elapsed;
+        TimerText.text = string.Format("{0:00}:{1:00}.{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+    }
+
+    // Displays the player's win time in WinCanvas under FinalTime
+    // (FinalTime is the text game object under WinCanvas)
+    public void Win()
+    {
+        timer.Stop();
+        TimeSpan ts = timer.Elapsed;
+        FinalTime.text = string.Format("{0:00}:{1:00}.{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+    }
+	/*public Text TimerText;
+    private float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Win () {
+		timer += Time.deltaTime;
+        TimerText.text = string.Format("{0:0}:{1:00}.{2:00}", timer / 60, timer % 60, timer * 100 % 100);
+	}*/
+}
---
+++ unity-animation/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    public Text timerText;
+    public Timer timerScript;
+    public GameObject player;
+    public GameObject timerCanvas;
+    public GameObject winCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       timerScript = player.GetComponent<Timer>();
+    }
+
+    // When player touches flag
+    void OnTriggerEnter(Collider collider)
+    {
+        // Update final win time
+        timerScript.Win();
+
+        // Hide the timer canvas
+        timerCanvas.gameObject.SetActive(false);
+
+        // Display the win canvas
+        winCanvas.gameObject.SetActive(true);
+
+    }
+}
+
+// (OLD) before win canvas
+/*public class WinTrigger : MonoBehaviour {
+
+	public GameObject player;
+	public int size = 60;
+	public Text timerText;
+	// Use this for initialization
+	private void OnTriggerEnter(Collider other)
+	{
+		player.GetComponent<Timer>().enabled = false;
+		timerText.fontSize = size;
+        timerText.color = Color.green;
+
+	}
+}*/
