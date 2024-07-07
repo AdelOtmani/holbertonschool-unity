ce0fa2216d2192017dd9f0ae6ad05eee3b6a102a
AdelOtmani <5023@holbertonschool.com>
2024-07-07 23:36:58 +0200

    unity platformer scripts and scene for project

--- unity-assets_ui/Assets/Scripts/PlayerController.cs
+++ unity-assets_ui/Assets/Scripts/PlayerController.cs
@@ -7,36 +7,37 @@ public class PlayerController : MonoBehaviour {
 	public float speed = 1500f;
 	public Rigidbody Rb;
 	public float jumpforce = 10;
-	public Vector3 startPosition;
+    public Vector3 startPosition;
     public Transform player;
 	// Use this for initialization
 	void Start () {
-		startPosition = player.position;
+        startPosition = player.position;
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-	    if (Input.GetKey(KeyCode.W))
+		if (Input.GetKey(KeyCode.D))
         {
-            Rb.AddForce(0, 0, speed * Time.deltaTime);
+            Rb.AddForce(speed * Time.deltaTime, 0, 0).Normalize;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            Rb.AddForce(-speed * Time.deltaTime, 0, 0);
+            Rb.AddForce(-speed * Time.deltaTime, 0, 0).Normalize;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Rb.AddForce(0, 0, speed * Time.deltaTime).Normalize;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            Rb.AddForce(0, 0, -speed * Time.deltaTime);
-        }
-		if (Input.GetKey(KeyCode.D))
-        {
-            Rb.AddForce(speed * Time.deltaTime, 0, 0);
+            Rb.AddForce(0, 0, -speed * Time.deltaTime).Normalize;
         }
 		if (Input.GetKey(KeyCode.Space))
 		{
-			Rb.AddForce(Vector3.up * jumpforce);
+			Rb.AddForce(Vector3.up * jumpforce).Normalize;
 		}
+
         if (player.position.y < -20)
         {
            player.position = new Vector3(startPosition.x, startPosition.y + 15, startPosition.z);
---
+++ unity-assets_ui/Assets/Scripts/WinMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Class for WinCanvas utility
+public class WinMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    // Open main menu
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+    public void Next()
+    {
+
+        Scene level = SceneManager.GetActiveScene();
+
+        if (level.name == "Level01")
+        {
+            SceneManager.LoadScene("Level02");
+        }
+        else if (level.name == "Level02")
+        {
+            SceneManager.LoadScene("Level03");
+        }
+        else
+        {
+            MainMenu();
+        }
+    }
+}
