a752222e03df81520ab089e7a7830baf52862cbc
AdelOtmani <5023@holbertonschool.com>
2024-07-31 07:50:03 +0200

    untity animation and readme

---
+++ unity-animation/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WinTrigger : MonoBehaviour {
+
+	public GameObject player;
+	public int size = 60;
+	public Text timerText;
+	// Use this for initialization
+	private void OnTriggerEnter(Collider other)
+	{
+		player.GetComponent<Timer>().enabled = false;
+		timerText.fontSize = size;
+        timerText.color = Color.green;
+
+	}
+}
