b796634c93bf9975d2d9ef3f1bb3e3397610e11a
AdelOtmani <5023@holbertonschool.com>
2024-08-02 10:39:06 +0200

    Duplicate repo unity-animation

---
+++ unity-audio/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+	public bool pauseGame;
+    private void Start()
+    {
+        pauseGame = false;
+        this.GetComponent<Canvas>().enabled = false;
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (pauseGame)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Pause: pauses the game
+    /// </summary>
+    public void Pause()
+    {
+        pauseGame = true;
+        Time.timeScale = 0;
+        this.GetComponent<Canvas>().enabled = true;
+    }
+
+    /// <summary>
+    /// resume game after pause
+    /// </summary>
+    public void Resume()
+    {
+        pauseGame = false;
+        Time.timeScale = 1;
+        this.GetComponent<Canvas>().enabled = false;
+    }
+
+    /// <summary>
+    /// Restart game
+    /// </summary>
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+    }
+
+    /// <summary>
+    /// main menu
+    /// </summary>
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(3);
+    }
+
+    /// <summary>
+    /// options menu
+    /// </summary>
+    public void Options()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Options");
+    }
+}
---
+++ unity-audio/README.md
@@ -0,0 +1,10 @@
+# holbertonschool's project: unity-assets_ui
+  credit: Models: Kenney's Nature Pack Extended
+            Link : https://www.kenney.nl/assets/nature-kit
+          The following attribution for the audio clips :
+            Kenney: https://kenney.nl/
+            Oculus Audio Pack: https://developer.oculus.com/downloads/package/oculus-audio-pack-1/
+            Mindful Audio: https://mindful-audio.com/
+            “Wallpaper”, “Cheery Monday” Kevin MacLeod (incompetech.com)
+            Licensed under Creative Commons: By Attribution 3.0
+                  http://creativecommons.org/licenses/by/3.0/
